e8575740c96ab09ac51750c6ee11978a7b140eff
Ryan Delaney <rrdelaney@outlook.com>
2022-10-22 14:21:01 -0700

    Finish landing page

--- components/header.tsx
+++ components/header.tsx
@@ -1,17 +1,108 @@
+import { Menu, Transition } from '@headlessui/react';
+import { Bars2Icon } from '@heroicons/react/20/solid';
 import cn from 'classnames';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { PropsWithChildren } from 'react';
+import {
+  AnchorHTMLAttributes,
+  forwardRef,
+  Fragment,
+  PropsWithChildren,
+} from 'react';
 
-function HeaderLink({ href, children }: PropsWithChildren<{ href: string }>) {
+const LINKS = [
+  { name: 'Philosophy', href: '/philosophy' },
+  { name: 'Decks', href: '/decks' },
+  { name: 'Stats', href: '/stats' },
+  { name: 'Articles', href: '/articles' },
+  { name: 'Watch', href: '/watch' },
+  { name: 'Play', href: '/play' },
+  { name: 'Track', href: '/track' },
+  { name: 'Team', href: '/team' },
+];
+
+const MobileNavigationMenuItemLink = forwardRef<
+  HTMLAnchorElement,
+  PropsWithChildren<AnchorHTMLAttributes<HTMLAnchorElement> & { href: string }>
+>((props, ref) => {
+  const { href, children, ...rest } = props;
+  return (
+    <Link href={href}>
+      <a ref={ref} {...rest}>
+        {children}
+      </a>
+    </Link>
+  );
+});
+
+function MobileNavigationMenuItem({
+  href,
+  children,
+}: PropsWithChildren<{ href: string }>) {
+  return (
+    <Menu.Item as={Fragment}>
+      {({ active }) => (
+        <MobileNavigationMenuItemLink href={href}>
+          <a
+            className={cn(
+              'group flex w-full items-center rounded-md p-2 text-lg font-bold',
+              active ? 'bg-violet-500 text-white' : 'text-gray-900'
+            )}
+          >
+            {children}
+          </a>
+        </MobileNavigationMenuItemLink>
+      )}
+    </Menu.Item>
+  );
+}
+
+function MobileNavigationMenu() {
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <Menu.Button className="md:hidden inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+        <Bars2Icon
+          className="h-8 w-8 text-violet-200 hover:text-violet-100"
+          aria-hidden="true"
+        />
+      </Menu.Button>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="md:hidden absolute right-0 mt-2 p-2 w-screen max-w-xs md:max-w-md origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          {LINKS.map(({ name, href }) => (
+            <MobileNavigationMenuItem key={href} href={href}>
+              {name}
+            </MobileNavigationMenuItem>
+          ))}
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  );
+}
+
+function DesktopHeaderLink({
+  href,
+  children,
+}: PropsWithChildren<{ href: string }>) {
   const router = useRouter();
 
   return (
     <Link href={href}>
       <a
-        className={cn('text-gray-300 transition-colors hover:text-gray-100', {
-          underline: router.pathname === href,
-        })}
+        className={cn(
+          'hidden md:inline-block text-gray-300 transition-colors hover:text-gray-100',
+          {
+            underline: router.pathname === href,
+          }
+        )}
       >
         {children}
       </a>
@@ -21,19 +112,17 @@ function HeaderLink({ href, children }: PropsWithChildren<{ href: string }>) {
 
 export function PageHeader() {
   return (
-    <div className="flex items-center space-x-4 p-6">
+    <nav className="flex items-center space-x-4 p-6">
       <Link href="/">
         <a className="flex-1 text-3xl font-bold">What is cEDH?</a>
       </Link>
 
-      <HeaderLink href="/philosophy">Philosophy</HeaderLink>
-      <HeaderLink href="/decks">Decks</HeaderLink>
-      <HeaderLink href="/stats">Stats</HeaderLink>
-      <HeaderLink href="/articles">Articles</HeaderLink>
-      <HeaderLink href="/watch">Watch</HeaderLink>
-      <HeaderLink href="/play">Play</HeaderLink>
-      <HeaderLink href="/track">Track</HeaderLink>
-      <HeaderLink href="/team">Team</HeaderLink>
-    </div>
+      <MobileNavigationMenu />
+      {LINKS.map(({ name, href }) => (
+        <DesktopHeaderLink key={href} href={href}>
+          {name}
+        </DesktopHeaderLink>
+      ))}
+    </nav>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import { useEffect, useRef, useState } from 'react';
 import { PageHeader } from '../components/header';
 
 function CedhBanner() {
   return (
     <div
-      className="md:pt-18 mx-auto flex max-w-screen-2xl flex-col items-start px-12 pt-12 md:mt-8 lg:mt-12 lg:pt-24"
+      className="md:pt-18 mx-auto mb-12 md:mb-20 flex max-w-screen-2xl flex-col items-start px-4 md:px-12 pt-12 md:mt-8 lg:mt-12 lg:pt-24"
       style={{
         backgroundImage: `url(/cedh-guide_card-banner_v2.png)`,
         backgroundSize: 'cover',
@@ -29,6 +31,119 @@ function CedhBanner() {
   );
 }
 
+function YouTubeEmbed({ src }: { src: string }) {
+  const iframeRef = useRef<HTMLIFrameElement | null>(null);
+  const [width, setWidth] = useState(0);
+  useEffect(() => {
+    function calculateParentWidth() {
+      const parentRect =
+        iframeRef.current?.parentElement?.getBoundingClientRect();
+
+      if (parentRect) {
+        const { width: parentWidth } = parentRect;
+        setWidth(Math.min(560, parentWidth - 20));
+      }
+    }
+
+    calculateParentWidth();
+    window.addEventListener('resize', calculateParentWidth);
+    return () => {
+      window.removeEventListener('resize', calculateParentWidth);
+    };
+  }, []);
+
+  return (
+    <iframe
+      ref={iframeRef}
+      width={`${width}`}
+      height={`${width * 0.5625}`}
+      src={src}
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    />
+  );
+}
+
+function Explanation() {
+  return (
+    <div className="mx-auto flex max-w-screen-xl flex-col items-center mb-12 md:mb-20">
+      <h1 className="text-4xl font-bold mb-8 text-center">
+        A Longer Explanation:
+      </h1>
+      <YouTubeEmbed src="https://www.youtube.com/embed/zH_ERuiwJWw" />
+    </div>
+  );
+}
+
+function ExampleGames() {
+  return (
+    <div className="mx-auto flex max-w-screen-xl flex-col items-center mb-12 md:mb-20">
+      <h1 className="text-4xl font-bold mb-8 text-center">Example Games:</h1>
+      <div className="w-full grid xl:grid-cols-2 gap-8 justify-center">
+        <YouTubeEmbed src="https://www.youtube.com/embed/zpnY5FpAlbY" />
+        <YouTubeEmbed src="https://www.youtube.com/embed/rqjYgspEaCk" />
+      </div>
+    </div>
+  );
+}
+
+function LearnMore() {
+  return (
+    <div className="mx-auto flex max-w-screen-xl flex-col items-center mb-12 md:mb-20 px-4">
+      <h1 className="text-4xl font-bold mb-8">
+        Want to learn more and start playing?
+      </h1>
+
+      <ol className="list-inside list-decimal space-y-2 pt-4 text-2xl">
+        <li>
+          <Link href="/philosophy">
+            <a className="underline">Read up</a>
+          </Link>{' '}
+          on the cEDH mindset and meta.
+        </li>
+        <li>
+          <Link href="/decks">
+            <a className="underline">Browse some decklists</a>
+          </Link>{' '}
+          for intriguing and proven strategies.
+        </li>
+        <li>
+          <Link href="/stats">
+            <a className="underline">Study the statistics</a>
+          </Link>{' '}
+          to learn patterns and common cards.
+        </li>
+        <li>
+          <Link href="/articles">
+            <a className="underline">Read evergreen articles</a>
+          </Link>{' '}
+          about cEDH.
+        </li>
+        <li>
+          <Link href="/watch">
+            <a className="underline">Watch videos</a>
+          </Link>{' '}
+          for stack wars, combat steps, and combos.
+        </li>
+        <li>
+          <Link href="/play">
+            <a className="underline">Play webcam games</a>
+          </Link>{' '}
+          in inclusive Discord communities.
+        </li>
+        <li>
+          <Link href="/track">
+            <a className="underline">Track your games</a>
+          </Link>{' '}
+          to help our community adapt and grow.
+        </li>
+      </ol>
+    </div>
+  );
+}
+
 export default function Home() {
   return (
     <>
@@ -37,6 +152,9 @@ export default function Home() {
       </Head>
       <PageHeader />
       <CedhBanner />
+      <Explanation />
+      <ExampleGames />
+      <LearnMore />
     </>
   );
 }
