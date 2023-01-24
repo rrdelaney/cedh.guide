2f9a0c37f61014fe1150a4aaf86008a960480e7d
Ryan Delaney <rrdelaney@outlook.com>
2023-01-24 00:33:24 -0800

    Add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
+import { Analytics } from '@vercel/analytics/react';
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
 function CedhGuideApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default appWithTranslation(CedhGuideApp);
